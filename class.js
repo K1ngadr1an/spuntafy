db71cea430c47f38998b1cbf99706e162f4a823e
Giann05 <gian2005gian@gmail.com>
2024-05-17 17:59:23 +0200

    sistemazione file class

--- class.js
+++ class.js
@@ -1,60 +1,117 @@
-class app {
-  users = [
-    (id = "1"),
-    (nikname = "gian"),
-    (nome = "gianluca"),
-    (cognome = "craparotta"),
-    (email = "gian@example.com"),
-    (password = "boh"),
-  ];
-  canzone = [
-    (id = "1"),
-    (titolo = "boh"),
-    (autore = "boh"),
-    (album = "boh"),
-    (genere = "boh"),
-    (anno = "boh"),
-    (durata = "boh"),
-    (idartista = "boh"),
-    (idalbum = "boh"),
-  ];
-  artista = [(id = 1), (nome = "boh"), (cognome = "boh")];
-  album = [
-    (id = "1"),
-    (titolo = "boh"),
-    (autore = "boh"),
-    (genere = "boh"),
-    (anno = "boh"),
-    (idautore = "boh"),
-  ];
-  playlist = [(id = "1"), (titolo = "boh"), (autore = "boh"), (iduser = "boh")];
+class App {
+  constructor() {
+    this.users = [
+      {
+        id: "1",
+        nickname: "gian",
+        nome: "gianluca",
+        cognome: "craparotta",
+        email: "gian@example.com",
+        password: "boh",
+      },
+    ];
+    this.canzoni = [
+      {
+        id: "1",
+        titolo: "boh",
+        autore: "boh",
+        genere: "boh",
+        anno: "boh",
+        durata: "boh",
+        idArtista: "boh",
+        idAlbum: "boh",
+        idPlaylist: "boh",
+      },
+    ];
+    this.artisti = [
+      {
+        id: "1",
+        nome: "boh",
+        cognome: "boh",
+      },
+    ];
+    this.album = [
+      {
+        id: "1",
+        titolo: "boh",
+        autore: "boh",
+        genere: "boh",
+        anno: "boh",
+        idArtista: "boh",
+      },
+    ];
+    this.playlist = [
+      {
+        id: "1",
+        titolo: "boh",
+        autore: "boh",
+        idUser: "boh",
+      },
+    ];
+    // this.chats = [];
+    // this.messages = [];
+  }
+}
 
-  /* chats = [];
-  messages = [];*/
-}
-class user {
-  constructor() {
-    this.users = null;
-  }
-}
-class canzone {
-  constructor() {
-    this.canzone = null;
-  }
-}
-class artista {
-  constructor() {
-    this.artista = null;
-  }
-}
-class album {
-  constructor() {
-    this.album = null;
+class User {
+  constructor(id, nickname, nome, cognome, email, password) {
+    this.id = id;
+    this.nickname = nickname;
+    this.nome = nome;
+    this.cognome = cognome;
+    this.email = email;
+    this.password = password;
   }
 }
 
-class playlist {
-  constructor() {
-    this.playlist = null;
+class Canzone {
+  constructor(
+    id,
+    titolo,
+    autore,
+    genere,
+    anno,
+    durata,
+    idArtista,
+    idAlbum,
+    idPlaylist
+  ) {
+    this.id = id;
+    this.titolo = titolo;
+    this.autore = autore;
+    this.genere = genere;
+    this.anno = anno;
+    this.durata = durata;
+    this.idArtista = idArtista;
+    this.idAlbum = idAlbum;
+    this.idPlaylist = idPlaylist;
+  }
+}
+
+class Artista {
+  constructor(id, nome, cognome) {
+    this.id = id;
+    this.nome = nome;
+    this.cognome = cognome;
+  }
+}
+
+class Album {
+  constructor(id, titolo, autore, genere, anno, idArtista) {
+    this.id = id;
+    this.titolo = titolo;
+    this.autore = autore;
+    this.genere = genere;
+    this.anno = anno;
+    this.idArtista = idArtista;
+  }
+}
+
+class Playlist {
+  constructor(id, titolo, autore, idUser) {
+    this.id = id;
+    this.titolo = titolo;
+    this.autore = autore;
+    this.idUser = idUser;
   }
 }
